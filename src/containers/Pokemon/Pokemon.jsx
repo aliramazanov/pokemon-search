da096bde5eb1b4ac8a9f04c6fea55a894191223b
Ali Ramazanov <aliasifzade@gmail.com>
2023-08-30 01:38:01 +0400

    Updated Pokemon

--- src/containers/Pokemon/Pokemon.jsx
+++ src/containers/Pokemon/Pokemon.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { GetPokemon } from "../../store/actions/dataActions";
 import _ from "lodash";
+import "./Pokemon.scss";
 
 export default function Pokemon() {
   const { pokemon: pokemonName } = useParams();
@@ -13,33 +14,47 @@ export default function Pokemon() {
     dispatch(GetPokemon(pokemonName));
   }, [dispatch, pokemonName]);
 
-  const ShowData = () => {
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  };
+
+  const showData = () => {
     if (!_.isEmpty(pokemonState.data[pokemonName])) {
       const pokeData = pokemonState.data[pokemonName];
+      const spriteImages = [
+        pokeData.sprites.front_default,
+        pokeData.sprites.back_default,
+        pokeData.sprites.front_shiny,
+        pokeData.sprites.back_shiny,
+      ];
+
       return (
         <div className={"pokemon-wrapper"}>
-          <div className={"item"}>
+          <div className="item">
             <h1>Sprites</h1>
-            <img src={pokeData.sprites.front_default} alt="" />
-            <img src={pokeData.sprites.back_default} alt="" />
-            <img src={pokeData.sprites.front_shiny} alt="" />
-            <img src={pokeData.sprites.back_shiny} alt="" />
+            <div className="floating-images">
+              {spriteImages.map((src, index) => (
+                <div key={index} className="floating-image">
+                  <img src={src} alt="" className="pokemon-image" />
+                </div>
+              ))}
+            </div>
           </div>
           <div className="item">
             <h1>Stats</h1>
-            {pokeData.stats.map((el) => {
-              return (
-                <p key={el.stat.name}>
-                  {el.stat.name} {el.base_stat}
-                </p>
-              );
-            })}
+            {pokeData.stats.map((el) => (
+              <p key={el.stat.name}>
+                {capitalizeFirstLetter(el.stat.name)}: {el.base_stat}
+              </p>
+            ))}
           </div>
           <div className="item">
             <h1>Abilities</h1>
-            {pokeData.abilities.map((el) => {
-              return <p key={el.ability.name}>{el.ability.name}</p>;
-            })}
+            {pokeData.abilities.map((el) => (
+              <p key={el.ability.name}>
+                {capitalizeFirstLetter(el.ability.name)}
+              </p>
+            ))}
           </div>
         </div>
       );
@@ -50,16 +65,16 @@ export default function Pokemon() {
     }
 
     if (pokemonState.errorMsg !== "") {
-      return <p>{pokemonState.errorMsg}</p>;
+      return <p>{capitalizeFirstLetter(pokemonState.errorMsg)}</p>;
     }
 
-    return <p>error getting pokemon</p>;
+    return <p>Error getting pokemon</p>;
   };
 
   return (
     <div className={"poke"}>
-      <h1>{pokemonName}</h1>
-      {ShowData()}
+      <h1 className="pokemon-name">{capitalizeFirstLetter(pokemonName)}</h1>
+      {showData()}
     </div>
   );
 }
